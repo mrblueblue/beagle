bea8a6b5219f60c3df5140214fe206d8e26ff63d
mrblueblue <grokut@gmail.com>
2017-04-21 20:57:36 -0700

    publish to hex

--- mix.exs
+++ mix.exs
@@ -2,12 +2,16 @@ defmodule Beagle.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :beagle,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :beagle,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      name: "Beagle",
+      source_url: "https://github.com/mrblueblue/beagle"
+   ]
   end
 
   # Configuration for the OTP application
@@ -30,4 +34,21 @@ defmodule Beagle.Mixfile do
   defp deps do
     [{:hound, "~> 1.0"}]
   end
+
+  defp description do
+   """
+   Add-ons for the Hound, the integration testing and browser automation library
+   """
+ end
+
+ defp package do
+   # These are the default files included in the package
+   [
+     name: :postgrex,
+     files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+     maintainers: ["Jonathan Huang"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/mrblueblue/beagle"}
+   ]
+ end
 end
